ac5e5594c08d97e65e5e02b9016ad1f4b2d8d6df
medjebb <92812220+medjebb@users.noreply.github.com>
2024-12-16 09:56:32 +0100

    adding the jwt authtification

--- Backend/foot-near-by-backend/src/auth/auth.controller.ts
+++ Backend/foot-near-by-backend/src/auth/auth.controller.ts
@@ -9,12 +9,26 @@ export class AuthController {
     constructor(private readonly authService: AuthService) { }
 
     @Post("register")
-    register(@Body() body: { email: string, password: string ,username:string },@Res() response: Response){
-            return this.authService.register(body.email, body.password,body.username,response);
+    async register(@Body() body: { email: string, password: string ,username:string },@Res() response: Response){
+        if(await this.authService.isEmailAlreadyUsed(body.email))
+            return response.status(400).json({ message: 'Invalid credentials' });
+        const user =await this.authService.register(body.email, body.password,body.username,response);
+        const loginResult = await this.authService.login(user);
+        return response.status(200).json( {
+            message: 'register successful',
+            access_token: loginResult.access_token,
+        });
     }
 
     @Post("login")
-    login(@Body() body: { email: string, password: string  }, @Res() response: Response){
-        return this.authService.login(body.email,body.password,response)
+    async login(@Body() body: { email: string, password: string  }, @Res() response: Response){
+        const user=await this.authService.valideUser(body.email,body.password);
+        if(!user)
+            return response.status(400).json({ message: 'Invalid credentials' });
+        const loginResult = await this.authService.login(user);
+        return response.status(200).json( {
+            message: 'Login successful',
+            access_token: loginResult.access_token,
+        });
     }
 }
