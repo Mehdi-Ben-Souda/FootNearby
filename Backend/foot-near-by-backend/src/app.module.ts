d8afd993ed8525e8825c89719e45edcc84780186
Ben Souda El Mehdi Salah <99757180+Mehdi-Ben-Souda@users.noreply.github.com>
2024-12-18 23:09:19 +0100

    implementing the pitch addition and modification

--- Backend/foot-near-by-backend/src/app.module.ts
+++ Backend/foot-near-by-backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 import {ConfigModule} from "@nestjs/config";
+import { PitchModule } from './pitch/pitch.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}), AuthModule, DatabaseModule, UserModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), AuthModule, DatabaseModule, UserModule, PitchModule],
   controllers: [],
   providers: [],
 })
