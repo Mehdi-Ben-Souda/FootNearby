a3749a1a0499420a2c4124ea41b0a9a066c85f8e
Ben Souda El Mehdi Salah <99757180+Mehdi-Ben-Souda@users.noreply.github.com>
2024-12-23 20:16:38 +0100

    adding location module

--- Backend/foot-near-by-backend/src/app.module.ts
+++ Backend/foot-near-by-backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 import {ConfigModule} from "@nestjs/config";
 import { PitchModule } from './pitch/pitch.module';
+import { LocationModule } from './location/location.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}), AuthModule, DatabaseModule, UserModule, PitchModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), AuthModule, DatabaseModule, UserModule, PitchModule, LocationModule],
   controllers: [],
   providers: [],
 })
